4d2df1ab7300c873e11ff7c244f11c542a56200d
Michal Konopka <michal.konopka@unity.pl>
2020-03-26 19:18:00 +0100

    import deps

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import 'fomantic-ui-css/semantic.min.css';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
