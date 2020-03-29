0bd0afc530bcd1ff019d3f9e20ba719ab3aa7941
Michal Konopka <michal.konopka@unity.pl>
2020-03-29 19:06:45 +0200

    fetch data

--- src/pages/contact-list-page.js
+++ src/pages/contact-list-page.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import axios from 'axios';
 import ContactList from '../components/contact-list';
+import { ContactContext } from '../context/contact-context';
 
-const ContactListPage = () => {
+export default function ContactListPage() {
+  const [state, dispatch] = useContext(ContactContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('http://localhost:3030/contacts');
+      dispatch({
+        type: 'FETCH_CONTACTS',
+        payload: response.data.data || response.data, // in case pagination is disabled
+      });
+    };
+    fetchData();
+  }, [dispatch]);
+
   return (
     <div>
       <h1>List of Contacts</h1>
-      <ContactList />
+      <ContactList contacts={state.contacts} />
     </div>
   );
-};
-
-export default ContactListPage;
+}
